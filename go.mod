7b3b8ebd5a6232acf79a14f1596c3b193f808cc9
Maxim Kozlov <maximko.ca@gmail.com>
2026-06-19 17:11:17 -0400

    Setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hydroko13/break_em_free
+
+go 1.26.4
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	fmt.Println("Break 'Em Free... made by Maxim Kozlov for the Ebitengine game jam 2026")
+
+
+	
+}
